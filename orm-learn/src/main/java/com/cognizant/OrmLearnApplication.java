04f3e9940efb07a71b091b4de428c36d7c8ba0e9
Avik Sarkar1 <avik.sarkar4@cognizant.com>
2020-04-23 10:28:30 +0530

    first commit

---
+++ orm-learn/src/main/java/com/cognizant/OrmLearnApplication.java
@@ -0,0 +1,66 @@
+package com.cognizant;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.cognizant.model.Country;
+import com.cognizant.service.CountryNotFoundException;
+import com.cognizant.service.CountryService;
+
+@SpringBootApplication
+public class OrmLearnApplication {
+	private static CountryService countryService;
+	private static final Logger LOGGER = LoggerFactory.getLogger(OrmLearnApplication.class);
+
+	public static void main(String[] args) throws CountryNotFoundException {
+		ApplicationContext context = SpringApplication.run(OrmLearnApplication.class, args);
+		countryService = context.getBean(CountryService.class);
+		LOGGER.info("Inside main");
+		testGetAllCountries();
+		getAllCountriesTest();
+		testAddCountry();
+		testUpdateCountry();
+		testDeleteCountry();
+	}
+
+	private static void testGetAllCountries() {
+		LOGGER.info("Start");
+		List<Country> countries = countryService.getAllCountries();
+		LOGGER.debug("countries={}", countries);
+		LOGGER.info("End");
+	}
+
+	private static void getAllCountriesTest() throws CountryNotFoundException {
+		LOGGER.info("Start");
+		Country country = countryService.findCountryByCode("AV");
+		LOGGER.debug("Country:{}", country);
+		LOGGER.info("End");
+	}
+	
+	private static void testAddCountry() {
+		LOGGER.info("Start");
+		Country country = new Country();
+		country.setCode("AV");
+		country.setName("Avik Sarkar");
+		countryService.addCountry(country);
+		LOGGER.info("End");
+	}
+	
+	private static void testUpdateCountry() throws CountryNotFoundException {
+		LOGGER.info("Start");
+		countryService.updateCountry("AV","AVIK");
+		countryService.findCountryByCode("AV");
+		LOGGER.info("End");
+	}
+	
+	private static void testDeleteCountry() {
+		LOGGER.info("Start");
+		countryService.deleteCountry("AV");
+		LOGGER.info("End");
+	}
+}
